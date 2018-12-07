2fcec7a33ac14d7c00fa85b972ac0c74d41ff881
SEtester <dengjiajie0109@gmail.com>
2018-12-07 16:41:55 +0800

    测试框架集成测试报告生成

--- run_case_method/run_all_cases.py
+++ run_case_method/run_all_cases.py
@@ -2,5 +2,13 @@
 
 from runtest_common import *
 
-run_dir(dir_test_case)
+from HTMLTestRunner import HTMLTestRunner
 
+# 指定测试用例为当前文件夹下的 test_case 目录
+discover = unittest.defaultTestLoader.discover(dir_test_case)
+now = time.strftime("%Y-%m-%d %H_%M_%S")
+filename = os.path.basename(os.path.abspath(__file__)).split('.')[0]
+file_abs_path = dir_test_report + '/' + filename + ' '+ now + 'result.html'
+fp = open(file_abs_path, 'wb')
+runner = HTMLTestRunner(stream=fp, title='测试报告', description='用例执行情况：')
+runner.run(discover)
--- run_case_method/runtest_common.py
+++ run_case_method/runtest_common.py
@@ -3,10 +3,12 @@
 import os
 import sys
 import unittest
+import time
 
 api_auto_test_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 member_path = api_auto_test_path + '/test_case/member'
 dir_test_case = api_auto_test_path + '/test_case'
+dir_test_report = api_auto_test_path + '/report'
 sys.path.insert(0, api_auto_test_path)
 sys.path.insert(0, member_path)
 
