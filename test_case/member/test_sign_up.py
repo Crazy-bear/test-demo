8c14e673ca8eeefb4ac60fffe0b0b34f5a3b3715
huangjunxiong <junxiong.huang@skysolutions.cn>
2019-01-10 14:53:14 +0800

    1.在test_sign_up.py中增加运营商手机号验证规则的测试用例： 中国移动：test_sign_up_mobile_phone_Operator_cmcc 中国电信：test_sign_up_mobile_phone_Operator_telecom 中国联通：test_sign_up_mobile_phone_Operator_unicom

--- test_case/member/test_sign_up.py
+++ test_case/member/test_sign_up.py
@@ -1,6 +1,7 @@
 # encoding:utf8
 import os
 import sys
+import re
 
 api_auto_test_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, api_auto_test_path)
@@ -38,7 +39,7 @@ class SignCase(unittest.TestCase):
         r = requests.post(URL_SIGN_UP, sign_up_pars)
         return r
 
-    @unittest.skipIf(api!='http://test-api.tianhangbox.net' , u'预发布环境跳过')
+    @unittest.skipIf(api != 'http://test-api.tianhangbox.net', u'预发布环境跳过')
     def test_sign_up_success(self):
         '''冒烟测试 注册'''
         mobile_phone = '18000000001'
@@ -275,6 +276,114 @@ class SignCase(unittest.TestCase):
             raise AssertionError
         finally:
             self.delect_member(self.db, mobile_phone)
+
+    def test_sign_up_mobile_phone_Operator_telecom(self):
+        '''注册 运营商中国电信号段:
+        133
+        141/149
+        153
+        173/174/177
+        180/181/189
+        191/199
+        '''
+        mobile_phone = [13323458741,
+                        14112545895, 14952124587,
+                        15325869521,
+                        17325621458, 17452154845, 17752658741,
+                        18025147854, 18125416325, 18952421545,
+                        19152485214, 19963254124
+                        ]
+        login_passwd = '123456'
+        login_passwd_md5 = md5_encryption(login_passwd)
+        for i in mobile_phone:
+                try:
+                    self.send_msg_code(i)
+                    code = self.query_msg_code(self.db, i)[0]['code']
+                    r = self.sign_up(i, login_passwd_md5, code)
+                    # print('sign',r.json())
+                    self.assertEqual(200, r.status_code)
+                    self.assertEqual('0', r.json()['ret'])
+                    self.assertEqual('成功', r.json()['msg'])
+                    self.assertEqual(str(i), r.json()['data']['mobilePhone'])
+                    login(i, login_passwd)
+                except AssertionError:
+                    raise AssertionError('手机号码错误！')
+                finally:
+                    self.delect_member(self.db, i)
+
+    def test_sign_up_mobile_phone_Operator_cmcc(self):
+        '''注册 运营商中国移动号段:
+        134/135/136/137/138/139
+        144/147/148
+        150/151/152/157/158/159
+        178
+        182/183/184/187/188
+        198
+        '''
+        mobile_phone = [13485252651, 13511475147, 13654874125, 13789456251, 13852416545, 13954154215,
+                        14452365214, 14785236951, 14852369512,
+                        15023125487, 15125632658, 15236598547, 15784215412, 15854125478, 15936251454,
+                        17852145214,
+                        18236514587, 18365245464, 18425154862, 18724513557, 18861548535,
+                        19865632547,
+                        ]
+        login_passwd = '123456'
+        login_passwd_md5 = md5_encryption(login_passwd)
+        for i in mobile_phone:
+                try:
+                    self.send_msg_code(i)
+                    code = self.query_msg_code(self.db, i)[0]['code']
+                    # 注册账号
+                    r = self.sign_up(i, login_passwd_md5, code)
+                    # print('sign',r.json())
+                    # 断言判断是否注册成功
+                    self.assertEqual(200, r.status_code)
+                    self.assertEqual('0', r.json()['ret'])
+                    self.assertEqual('成功', r.json()['msg'])
+                    self.assertEqual(str(i), r.json()['data']['mobilePhone'])
+                    # 登录账号，判断是否可以登录
+                    login(i, login_passwd)
+                except AssertionError:
+                    raise AssertionError
+                finally:
+                    self.delect_member(self.db, i)
+
+    def test_sign_up_mobile_phone_Operator_unicom(self):
+        '''注册 运营商中国联通号段:
+        130/131/132
+        145/146
+        155/156
+        166
+        175/176
+        185/186
+        '''
+        mobile_phone = [13025896521, 13120254587, 13232365214,
+                        14526589512, 14685212547,
+                        15524258951, 15623587414,
+                        16662521888,
+                        17525859664, 17632584785,
+                        18521478523, 18625415784
+                        ]
+        login_passwd = '123456'
+        login_passwd_md5 = md5_encryption(login_passwd)
+        for i in mobile_phone:
+                try:
+                    self.send_msg_code(i)
+                    code = self.query_msg_code(self.db, i)[0]['code']
+                    # 注册账号
+                    r = self.sign_up(i, login_passwd_md5, code)
+                    # print('sign',r.json())
+                    # 断言判断是否注册成功
+                    self.assertEqual(200, r.status_code)
+                    self.assertEqual('0', r.json()['ret'])
+                    self.assertEqual('成功', r.json()['msg'])
+                    self.assertEqual(str(i), r.json()['data']['mobilePhone'])
+                    # 登录账号，判断是否可以登录
+                    login(i, login_passwd)
+                except AssertionError:
+                    raise AssertionError
+                finally:
+                    self.delect_member(self.db, i)
 
     def test_sign_up_login_passwd_null(self):
         '''注册 密码为：空'''
